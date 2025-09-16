827443d073c27addcafd4c89f6f259eadd1fd381
ryo ishida <r.ishida.cingroup2012@gmail.com>
2025-09-16 13:34:08 +0900

    pg_practice.sql 修正版

--- pg_practice.sql
+++ pg_practice.sql
@@ -4,9 +4,10 @@ CREATE DATABASE practice;
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL DEFAULT '名無し',
   age INT NOT NULL DEFAULT 0,
   gender VARCHAR(10) NOT NULL DEFAULT 'Other'
+   CHECK (gender IN ('Man', 'Woman', 'Other'))
 );
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
